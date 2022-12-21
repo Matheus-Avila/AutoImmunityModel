f4b8675918f05b991b6e0c89d634414b20825009
Matheus-Avila <matheusavila1997@gmail.com>
2022-12-21 15:40:53 -0300

    SA code done!

--- sensitivity_analysis/sa.py
+++ sensitivity_analysis/sa.py
@@ -4,6 +4,54 @@ import os
 from SALib.sample import saltelli
 from SALib.analyze import sobol
 
+def printResult(indexes, title, fileName):
+    labels = ["d_mic",
+    "d_anti",
+    "d_dc",
+    "d_da",
+    "d_t_cit",
+    "chi",
+
+    "mu_dc",
+    "mu_m",
+    "r_m",
+    "r_t",
+    "lamb_f_m",
+    "b_d",
+
+    "gamma_D",
+    "gamma_F",
+    "gamma_T",
+    
+    "cMic",
+    "cCDc",
+    "cADc",
+    "cDl",
+    "cF",
+    
+    "alpha_T_h",
+    "alpha_T_c",
+    "alpha_B",
+    "alpha_P",
+
+    "b_T",
+    "b_Tc",
+    "b_rho",
+    "b_rho_b",
+    "b_rho_p",
+    "rho_T",
+    "rho_Tc",
+    "rho_B",
+    "rho_P",
+    "rho_F"]
+    # plt.bar(labels, indexes, color ='maroon')
+    plt.title(title)
+    plt.barh(labels, indexes, color ='maroon')
+    # plt.show()
+    plt.savefig("./sensitivity_analysis/"+ fileName +".png", dpi = 900)
+    plt.clf()
+
+
 def modelo(d_mic, d_anti, d_dc, d_da, d_t_cit, chi, mu_dc, mu_m, r_m, r_t, lamb_f_m, b_d, gamma_D, gamma_F, gamma_T,
 cMic, cCDc, cADc, cDl, cF, alpha_T_h, alpha_T_c, alpha_B, alpha_P, b_Th, b_Tc, b_rho, b_rho_b, b_rho_p, rho_Th, rho_Tc, rho_B, rho_P, rho_F):
     #Escrever parametros em um txt
@@ -58,7 +106,6 @@ b_Tc_mean = 0.001
 b_rho_mean = 0.6
 b_rho_b_mean = 3
 b_rho_p_mean = 1.02
-
 rho_T_mean = 2
 rho_Tc_mean = 2
 rho_B_mean = 11
@@ -109,6 +156,7 @@ problem = {
         'rho_T',
         'rho_Tc',
         'rho_B',
+        'rho_P',
         'rho_F'
     ],
     'bounds': [
@@ -169,9 +217,15 @@ problem_teste = {
 
 def RunSA():
     print("Running Model")
-    sample = saltelli.sample(problem, 4, calc_second_order=False)
+    numSamples = 4
+    calcSecondOrder=False
+    sample = saltelli.sample(problem, numSamples, calc_second_order=calcSecondOrder)
     Y = np.empty([sample.shape[0]])
-
+    if calcSecondOrder:
+        numOutPuts = numSamples*(2 * len(sample[0]) + 2)
+    else:
+        numOutPuts = numSamples*(len(sample[0]) + 2)
+    
     inputFile = open("sample.txt", "w")
     for samp in sample:
         inputFile.write(str(samp) + "\n")
@@ -180,15 +234,14 @@ def RunSA():
     # evaluate the model for eah point in the input sample
     for i in range(len(Y)):
         x = sample[i]
-        Y[i] = modelo(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[2], x[13], x[14], x[15], x[16], x[17], x[18], x[19], x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28], x[29], x[30], x[31], x[32], x[33])
+        # Y[i] = modelo(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[2], x[13], x[14], x[15], x[16], x[17], x[18], x[19], x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28], x[29], x[30], x[31], x[32], x[33])
 
-    output_file = np.zeros(640)
+    output_file = np.zeros(numOutPuts)
     i = 0
-    with open("returns.txt", "r") as f:
+    with open("./sensitivity_analysis/SAalloutput.txt", "r") as f:
         # output_file = f.readlines()
         for y in f.read().split("\n"):
-            print(y)
-            if i == 640:
+            if i == numOutPuts:
                 break
             if type(y) == str:
                 output_file[i] = float(y)
@@ -197,84 +250,19 @@ def RunSA():
     # print(output_file)
 
     # estimate the sensitivity indices using Sobol's method
-    sensitivity = sobol.analyze(problem_teste, output_file, calc_second_order=False)
+    sensitivity = sobol.analyze(problem, output_file, calc_second_order=calcSecondOrder)
 
     # firstorder indices
     print("First-order or main effect indices")
     print(sensitivity['S1'])
-    # interpretation: x1 contributes to half of the 
-    # total output uncertainty
 
     # higher-order indices
     print("Higher-order or total (interactions) indices")
     print(sensitivity['ST'])
-    return sensitivity
 
-def ReadSensitivity():
-    sensitivity = {
-        'S1': [8.10470350e-02, 1.30161888e-06, -1.87588328e-08, -7.98065622e-09,
-    1.19157601e-04, 2.15319320e-02, -1.79993643e-07, 8.87319458e-01,
-    2.32798953e-05, -1.89251553e-05, -7.30988451e-06, -5.04483338e-07,
-    -1.36386018e-08, -4.24069663e-07, -3.79414154e-05, 5.71591451e-03,
-    -3.35283511e-08, -5.36640357e-07, -3.59938269e-07 -1.39886999e-08,
-    -9.11781785e-09, 6.00908583e-05, -3.75227026e-08, -3.01878232e-06,
-    -7.21745691e-08, 1.92672821e-08, -5.38780976e-07, -1.92658619e-08,
-    7.56544201e-07, -2.69817677e-07, -5.35171487e-08, 3.66348520e-08,
-    -2.89514282e-07, -1.81924591e-06],
-        'ST': [8.43824923e-02, 4.91991490e-09, 1.98017551e-09, 2.44353816e-10,
-    2.93806199e-06, 2.18838369e-02, 2.86261721e-10, 8.83197561e-01,
-    2.24520567e-05, 1.47011920e-05, 3.36275160e-08, 2.20992648e-09,
-    2.61491383e-13, 4.33716384e-10, 6.67356909e-06, 3.61700152e-03,
-    6.94283658e-13, 2.11023394e-10, 1.09136294e-10, 3.04907957e-13,
-    6.27420006e-09, 4.96628909e-05, 1.24207262e-11, 7.78751342e-09,
-    1.04881928e-11, 4.06162426e-13, 1.40977732e-08, 4.68120699e-12,
-    5.42575796e-09, 3.96713026e-11, 4.99328746e-13, 2.08652193e-09,
-    5.66606580e-09, 5.10866780e-09]
-    }
+    printResult(sensitivity['S1'], 'First Order', 'firstOrder')
+    printResult(sensitivity['ST'], 'Total Order', 'totalOrder')
     return sensitivity
 
 sensitivity = RunSA()
-# sensitivity = ReadSensitivity()
 
-def printResult(indexes):
-    labels = ["d_mic",
-    "d_anti",
-    "d_dc",
-    "d_da",
-    "d_t_cit",
-    "chi",
-    "mu_dc",
-    "mu_m",
-    "r_m",
-    "r_t",
-    "lamb_f_m",
-    "b_d",
-    "gamma_D",
-    "gamma_F",
-    "gamma_T",
-    "cMic",
-    "cCDc",
-    "cADc",
-    "cDl",
-    "cF",
-    "alpha_T_h",
-    "alpha_T_c",
-    "alpha_B",
-    "alpha_P",
-    "b_T",
-    "b_Tc",
-    "b_rho",
-    "b_rho_b",
-    "b_rho_p",
-    "rho_T",
-    "rho_Tc",
-    "rho_B",
-    "rho_F"]
-    # plt.bar(labels, indexes, color ='maroon')
-    plt.barh(labels, indexes, color ='maroon')
-    plt.show()
-    # plt.savefig("SAOaT-Results.png", dpi = 900)
-    plt.clf()
-
-printResult(sensitivity['S1'])
-printResult(sensitivity['ST'])
