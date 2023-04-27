3520138041dba01525a317b907943b5a93cd6b15
Matheus-Avila <matheusavila1997@gmail.com>
2023-04-27 15:50:43 -0300

    changing hx

--- mainOMP.c
+++ mainOMP.c
@@ -55,7 +55,6 @@ structParameters ParametersInitialize(){
     params.V_LN = 160;
     params.V_BV = 0;
     params.V_PV = 0;
-
     return params;
 }
 
@@ -75,7 +74,7 @@ int main(int argc, char* argv[]){
     printf("Comecei o main\n");
     clearPhgTxt();
     int tot_thr = strtol(argv[1], NULL, 10);
-    float ht = 0.0002, hx = 0.05;
+    float ht = 0.0002, hx = 0.5;
     int numFigs = 28, numPointsLN = 1000, time = 28, space = 20;
     structParameters parameters = ParametersInitialize();
     structModel model = ModelInitialize(parameters, tot_thr, ht, hx, time, space, numFigs, numPointsLN);
