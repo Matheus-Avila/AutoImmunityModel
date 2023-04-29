55eec24a868b69cb351c6601dd8a1b7a20132526
Matheus-Avila <matheusavila1997@gmail.com>
2023-04-29 01:30:26 -0300

    Migration terms verified

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-file_data = np.loadtxt("result/matrix/antibody28.0.txt")
+file_data = np.loadtxt("result/matrix/activatedDC28.0.txt")
 
 print("Final value cd8 {}".format(np.sum(file_data)))
